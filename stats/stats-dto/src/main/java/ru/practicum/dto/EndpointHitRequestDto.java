8e13284fa8dfd68096181ddb0342ac1ae881ed39
Vladimir <v-e-komov@yandex.ru>
2023-03-01 09:59:18 +0300

    feat: add stats client

--- stats/stats-dto/src/main/java/ru/practicum/dto/EndpointHitRequestDto.java
+++ stats/stats-dto/src/main/java/ru/practicum/dto/EndpointHitRequestDto.java
@@ -1,12 +1,18 @@
 package ru.practicum.dto;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import javax.validation.constraints.NotBlank;
 import java.sql.Timestamp;
 
 @Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class EndpointHitRequestDto {
     private long id;
     @NotBlank
